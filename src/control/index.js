4565a9660247ac7b6df5258b180f7703f0bb8344
alex4401 <rylatgl@gmail.com>
2022-06-09 03:12:07 +0200

    Stripping down the build for ARK Wiki
    Unlinking features we do not use or need at the moment. This reduces the footprint
    by 30kB uncompressed.

--- src/control/index.js
+++ src/control/index.js
@@ -1,17 +1,17 @@
 import {Control, control} from './Control';
-import {Layers, layers} from './Control.Layers';
+//import {Layers, layers} from './Control.Layers';
 import {Zoom, zoom} from './Control.Zoom';
-import {Scale, scale} from './Control.Scale';
-import {Attribution, attribution} from './Control.Attribution';
+//import {Scale, scale} from './Control.Scale';
+//import {Attribution, attribution} from './Control.Attribution';
 
-Control.Layers = Layers;
+//Control.Layers = Layers;
 Control.Zoom = Zoom;
-Control.Scale = Scale;
-Control.Attribution = Attribution;
+//Control.Scale = Scale;
+//Control.Attribution = Attribution;
 
-control.layers = layers;
+//control.layers = layers;
 control.zoom = zoom;
-control.scale = scale;
-control.attribution = attribution;
+//control.scale = scale;
+//control.attribution = attribution;
 
 export {Control, control};
