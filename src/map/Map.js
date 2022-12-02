ed505a01bea4e2228a250a38f7fdd9f26c68cd17
alex4401 <rylatgl@gmail.com>
2022-12-02 05:30:49 +0100

    Cut geolocation out

--- src/map/Map.js
+++ src/map/Map.js
@@ -3,7 +3,7 @@ import {Evented} from '../core/Events';
 import {Simple} from '../geo/crs/CRS.Simple';
 import {Point, toPoint} from '../geometry/Point';
 import {Bounds, toBounds} from '../geometry/Bounds';
-import {LatLng, toLatLng} from '../geo/LatLng';
+import {toLatLng} from '../geo/LatLng';
 import {LatLngBounds, toLatLngBounds} from '../geo/LatLngBounds';
 import Browser from '../core/Browser';
 import * as DomEvent from '../dom/DomEvent';
@@ -611,113 +611,6 @@ export const Map = Evented.extend({
 			this.fire('viewreset');
 		}
 		return this._stop();
-	},
-
-	// @section Geolocation methods
-	// @method locate(options?: Locate options): this
-	// Tries to locate the user using the Geolocation API, firing a [`locationfound`](#map-locationfound)
-	// event with location data on success or a [`locationerror`](#map-locationerror) event on failure,
-	// and optionally sets the map view to the user's location with respect to
-	// detection accuracy (or to the world view if geolocation failed).
-	// Note that, if your page doesn't use HTTPS, this method will fail in
-	// modern browsers ([Chrome 50 and newer](https://sites.google.com/a/chromium.org/dev/Home/chromium-security/deprecating-powerful-features-on-insecure-origins))
-	// See `Locate options` for more details.
-	locate(options) {
-
-		options = this._locateOptions = Util.extend({
-			timeout: 10000,
-			watch: false
-			// setView: false
-			// maxZoom: <Number>
-			// maximumAge: 0
-			// enableHighAccuracy: false
-		}, options);
-
-		if (!('geolocation' in navigator)) {
-			this._handleGeolocationError({
-				code: 0,
-				message: 'Geolocation not supported.'
-			});
-			return this;
-		}
-
-		const onResponse = this._handleGeolocationResponse.bind(this),
-		    onError = this._handleGeolocationError.bind(this);
-
-		if (options.watch) {
-			this._locationWatchId =
-			        navigator.geolocation.watchPosition(onResponse, onError, options);
-		} else {
-			navigator.geolocation.getCurrentPosition(onResponse, onError, options);
-		}
-		return this;
-	},
-
-	// @method stopLocate(): this
-	// Stops watching location previously initiated by `map.locate({watch: true})`
-	// and aborts resetting the map view if map.locate was called with
-	// `{setView: true}`.
-	stopLocate() {
-		if (navigator.geolocation && navigator.geolocation.clearWatch) {
-			navigator.geolocation.clearWatch(this._locationWatchId);
-		}
-		if (this._locateOptions) {
-			this._locateOptions.setView = false;
-		}
-		return this;
-	},
-
-	_handleGeolocationError(error) {
-		if (!this._container._leaflet_id) { return; }
-
-		const c = error.code,
-		    message = error.message ||
-		            (c === 1 ? 'permission denied' :
-		            (c === 2 ? 'position unavailable' : 'timeout'));
-
-		if (this._locateOptions.setView && !this._loaded) {
-			this.fitWorld();
-		}
-
-		// @section Location events
-		// @event locationerror: ErrorEvent
-		// Fired when geolocation (using the [`locate`](#map-locate) method) failed.
-		this.fire('locationerror', {
-			code: c,
-			message: `Geolocation error: ${message}.`
-		});
-	},
-
-	_handleGeolocationResponse(pos) {
-		if (!this._container._leaflet_id) { return; }
-
-		const lat = pos.coords.latitude,
-		    lng = pos.coords.longitude,
-		    latlng = new LatLng(lat, lng),
-		    bounds = latlng.toBounds(pos.coords.accuracy * 2),
-		    options = this._locateOptions;
-
-		if (options.setView) {
-			const zoom = this.getBoundsZoom(bounds);
-			this.setView(latlng, options.maxZoom ? Math.min(zoom, options.maxZoom) : zoom);
-		}
-
-		const data = {
-			latlng,
-			bounds,
-			timestamp: pos.timestamp
-		};
-
-		for (const i in pos.coords) {
-			if (typeof pos.coords[i] === 'number') {
-				data[i] = pos.coords[i];
-			}
-		}
-
-		// @event locationfound: LocationEvent
-		// Fired when geolocation (using the [`locate`](#map-locate) method)
-		// went successfully.
-		this.fire('locationfound', data);
 	},
 
 	// TODO Appropriate docs section?
