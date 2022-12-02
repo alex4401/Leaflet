e7603172c3e24528e781b38997652c591fe23bb8
alex4401 <rylatgl@gmail.com>
2022-12-02 05:22:12 +0100

    Drop factories

--- src/layer/marker/DivIcon.js
+++ src/layer/marker/DivIcon.js
@@ -66,9 +66,3 @@ export const DivIcon = Icon.extend({
 		return null;
 	}
 });
-
-// @factory L.divIcon(options: DivIcon options)
-// Creates a `DivIcon` instance with the given options.
-export function divIcon(options) {
-	return new DivIcon(options);
-}
