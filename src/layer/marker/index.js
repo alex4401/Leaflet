a8b092cbbc864d92e84cc24c13ddd361cbf5b05d
alex4401 <rylatgl@gmail.com>
2022-12-24 21:58:33 +0100

    Drop the IconDefault class

--- src/layer/marker/index.js
+++ src/layer/marker/index.js
@@ -1,7 +1,7 @@
 import {Icon} from './Icon';
 export {icon} from './Icon';
-import {IconDefault} from './Icon.Default';
-Icon.Default = IconDefault;
+// import {IconDefault} from './Icon.Default';
+// Icon.Default = IconDefault;
 export {Icon};
 
 export {DivIcon} from './DivIcon';
