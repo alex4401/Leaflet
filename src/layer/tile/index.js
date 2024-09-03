6bc5d4ded7ee4c97ad3097978524e2ae5586d78c
alex4401 <rylatgl@gmail.com>
2024-09-03 17:29:50 +0200

    Don't bundle TileLayer, we don't use it and it increases bundle size

--- src/layer/tile/index.js
+++ src/layer/tile/index.js
@@ -1,6 +1,10 @@
 export {GridLayer, gridLayer} from './GridLayer.js';
+// begin indie.io: don't include TileLayer to reduce bundle size
+/*
 import {TileLayer, tileLayer} from './TileLayer.js';
 import {TileLayerWMS, tileLayerWMS} from './TileLayer.WMS.js';
 TileLayer.WMS = TileLayerWMS;
 tileLayer.wms = tileLayerWMS;
 export {TileLayer, tileLayer};
+*/
+// end indie.io
